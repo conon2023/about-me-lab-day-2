393d42589729de1462010fd13ecd15ac27e918f7
CHIDI ONONYE <fononye2020@gmail.com>
2023-06-17 16:53:18 +0100

    did not uncomment some of my comments before pushing

--- app.js
+++ app.js
@@ -1,95 +1,95 @@
-// 'use strict';
+'use strict';
 
-// var Name = prompt("Hello! What is your name?");
-// var result = confirm("Welcome to My Site, " + Name + "!");
+var Name = prompt("Hello! What is your name?");
+var result = confirm("Welcome to My Site, " + Name + "!");
 
-// let userInput = prompt("Ready for an 'About Me' game quiz? Yes/Y or No/N ONLY");
+let userInput = prompt("Ready for an 'About Me' game quiz? Yes/Y or No/N ONLY");
 
-//   if (userInput.toLowerCase() === "y" || userInput.toLowerCase() === "yes") {
-//     // User chose yes
-//     alert("You selected yes!");
-//   } else if (userInput.toUpperCase() === "N" || userInput.toUpperCase() === "NO") {
-//     // User chose no
-//     alert("You selected no!");
-//   } else {
-//     // Invalid input
-//     alert("Please enter 'Yes' or 'Y' for Yes, OR 'No' or 'N' for No.");
-//   }
+  if (userInput.toLowerCase() === "y" || userInput.toLowerCase() === "yes") {
+    // User chose yes
+    alert("You selected yes!");
+  } else if (userInput.toUpperCase() === "N" || userInput.toUpperCase() === "NO") {
+    // User chose no
+    alert("You selected no!");
+  } else {
+    // Invalid input
+    alert("Please enter 'Yes' or 'Y' for Yes, OR 'No' or 'N' for No.");
+  }
 
-// // Counter variable to track the question number
-// var questionNumber = 0;
+// Counter variable to track the question number
+var questionNumber = 0;
 
-// // Function to prompt the user with a question and validate the answer
-// function promptQuestion(question, correctAnswer) {
-//   const userAnswer = prompt(question);
+// Function to prompt the user with a question and validate the answer
+function promptQuestion(question, correctAnswer) {
+  const userAnswer = prompt(question);
   
-//   // Normalize user input
-//   const normalizedUserAnswer = userAnswer.toUpperCase();
+  // Normalize user input
+  const normalizedUserAnswer = userAnswer.toUpperCase();
 
-//   // Normalize correct answer
-//   const normalizedCorrectAnswer = correctAnswer.toUpperCase();
+  // Normalize correct answer
+  const normalizedCorrectAnswer = correctAnswer.toUpperCase();
 
-//   // Validate the answer
-//   if (normalizedUserAnswer === normalizedCorrectAnswer || normalizedUserAnswer === normalizedCorrectAnswer.substring(0, 1)) {
-//     alert("Fantastic! It seems like you know me!");
-//   } else {
-//     alert("Oops! You can do better next time!");
-//   }
-// }
+  // Validate the answer
+  if (normalizedUserAnswer === normalizedCorrectAnswer || normalizedUserAnswer === normalizedCorrectAnswer.substring(0, 1)) {
+    alert("Fantastic! It seems like you know me!");
+  } else {
+    alert("Oops! You can do better next time!");
+  }
+}
 
-// // Continue with the other questions and prompts inside the loop
-// while (questionNumber < 5) {
-//   if (questionNumber === 0) {
-//     promptQuestion("I am from Nigeria?", "YES");
-//     questionNumber++;
-//   }
+// Continue with the other questions and prompts inside the loop
+while (questionNumber < 5) {
+  if (questionNumber === 0) {
+    promptQuestion("I am from Nigeria?", "YES");
+    questionNumber++;
+  }
 
-//   if (questionNumber === 1) {
-//     promptQuestion("Do I have a Masters in Education?", "YES");
-//     questionNumber++;
-//   }
+  if (questionNumber === 1) {
+    promptQuestion("Do I have a Masters in Education?", "YES");
+    questionNumber++;
+  }
 
-//   if (questionNumber === 2) {
-//     promptQuestion("Have I worked in the Medical Industry?", "NO");
-//     questionNumber++;
-//   }
+  if (questionNumber === 2) {
+    promptQuestion("Have I worked in the Medical Industry?", "NO");
+    questionNumber++;
+  }
 
-//   if (questionNumber === 3) {
-//     promptQuestion("Do I love family life?", "YES");
-//     questionNumber++;
-//   }
+  if (questionNumber === 3) {
+    promptQuestion("Do I love family life?", "YES");
+    questionNumber++;
+  }
 
-//   if (questionNumber === 4) {
-//     promptQuestion("Do I have future plans for mountain climbing?", "NO");
-//     questionNumber++;
-//   }
-// }
+  if (questionNumber === 4) {
+    promptQuestion("Do I have future plans for mountain climbing?", "NO");
+    questionNumber++;
+  }
+}
 
-// // Time for number guessing game
-// let correctNumber = 4;
-// let attempt = 4;
-// let guessedCorrectly = false;
+// Time for number guessing game
+let correctNumber = 4;
+let attempt = 4;
+let guessedCorrectly = false;
 
-// while (attempt > 0) {
-//   let userInput1 = prompt("Guess the number:");
-//   let userGuess = parseInt(userInput1);
+while (attempt > 0) {
+  let userInput1 = prompt("Guess the number:");
+  let userGuess = parseInt(userInput1);
   
-//   if (userGuess < correctNumber) {
-//     alert("Wrong! Answer is too low");
-//   } else if (userGuess > correctNumber) {
-//     alert("Wrong! Answer is too high");
-//   } else if (userGuess === correctNumber) {
-//     alert("Fantastic! You are amazing!");
-//     guessedCorrectly = true; // Update guessedCorrectly to true
-//     break; // Exit the loop since the answer is correct
-//   }
+  if (userGuess < correctNumber) {
+    alert("Wrong! Answer is too low");
+  } else if (userGuess > correctNumber) {
+    alert("Wrong! Answer is too high");
+  } else if (userGuess === correctNumber) {
+    alert("Fantastic! You are amazing!");
+    guessedCorrectly = true; // Update guessedCorrectly to true
+    break; // Exit the loop since the answer is correct
+  }
   
-//   attempt--;
+  attempt--;
 
-//   if (attempt === 0 && !guessedCorrectly) {
-//     alert("Sorry! Your chances are up. The correct number is 4");
-//   }
-// }
+  if (attempt === 0 && !guessedCorrectly) {
+    alert("Sorry! Your chances are up. The correct number is 4");
+  }
+}
 
 var correctAnswers = ["apple", "banana", "orange", "grape", "kiwi"];
 var attempts = 6;
